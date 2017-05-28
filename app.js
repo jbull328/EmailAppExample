683361ac81bb8e56df354000011730824b63b90a
John Bull <jackbull328@gmail.com>
2017-05-27 22:08:03 -0700

    added Sparkpost Test

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+var SparkPost = require('sparkpost');
+var client = new SparkPost(); // uses process.env.SPARKPOST_API_KEY
+var from = 'test@' + process.env.SPARKPOST_SANDBOX_DOMAIN; // 'test@sparkpostbox.com'
+var txObject = {
+	campaign: 'first-mailing',
+	from: from,
+	subject: 'Hello from node-sparkpost',
+	html: '<p>Hello world</p>',
+	text: 'Hello world',
+	substitutionData: {
+		"<YOUR_SUBSTITUTION_DATA_KEY_VALUE_PAIRS_HERE>": "value"
+	},
+	recipients: [
+		"<YOUR_LIST_OF_RECIPIENT_OBJECTS_HERE>"
+	]
+};
 app.get("/", function(req, res) {
   res.render('home.ejs');
 });
